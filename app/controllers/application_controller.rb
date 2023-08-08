5dfd7a08ef36cd199d8dc926383120631b91869f
Dico Diaz <dicodiaz@gmail.com>
2023-08-07 19:06:47 -0500

    Add Users::SessionsController and Users::RegistrationsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::API
-  include ActionController::MimeResponds
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  respond_to :json
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :password)
+    end
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:email, :password)
+    end
+  end
 end
