050d7df0f43ea37f5ef642235bd829ffe717c8b0
Dico Diaz <dicodiaz@gmail.com>
2023-08-08 15:52:05 -0500

    Add numericality validations to Videogame and Reservation

--- app/models/videogame.rb
+++ app/models/videogame.rb
@@ -4,5 +4,5 @@ class Videogame < ApplicationRecord
   validates :name, presence: true
   validates :photo, presence: true
   validates :description, presence: true
-  validates :price_per_day, presence: true
+  validates :price_per_day, presence: true, numericality: { greater_than: 0 }
 end
