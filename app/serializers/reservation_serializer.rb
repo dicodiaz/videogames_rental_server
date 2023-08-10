faadf5946586c47d7e989c6366c2d1bd046a791a
Dico Diaz <dicodiaz@gmail.com>
2023-08-10 13:58:59 -0500

    Add :created_at to ReservationSerializer

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,6 +1,6 @@
 class ReservationSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :user_id, :days, :total_price
+  attributes :id, :user_id, :days, :total_price, :created_at
   attribute :videogame do |reservation|
     VideogameSerializer.new(reservation.videogame).serializable_hash[:data][:attributes]
   end
