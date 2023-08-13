b6838aacebc49d6ac49558e4dbac2e7264cd2290
Dico Diaz <dicodiaz@gmail.com>
2023-08-13 13:03:08 -0500

    Add user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    User.create(name: 'user name', address: 'user address', email: 'user_email@mail.com', password: 'user_password')
+  end
+
+  context 'validations' do
+    it('should be valid') do
+      expect(user).to be_valid
+    end
+
+    it 'should be invalid when name is not present' do
+      user.name = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'should be invalid when address is not present' do
+      user.address = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'should be invalid when email is not present' do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+  end
 end
