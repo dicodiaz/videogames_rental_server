b97777f58df221cae219e80cc6cf7fe6fdbad189
Dico Diaz <dicodiaz@gmail.com>
2023-08-13 13:22:42 -0500

    Fix routing specs

--- spec/routing/reservations_routing_spec.rb
+++ spec/routing/reservations_routing_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 RSpec.describe ReservationsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/reservations').to route_to('reservations#index')
+      expect(get: '/reservations').to route_to('reservations#index', format: :json)
     end
 
     it 'routes to #show' do
-      expect(get: '/reservations/1').to route_to('reservations#show', id: '1')
+      expect(get: '/reservations/1').to route_to('reservations#show', id: '1', format: :json)
     end
 
     it 'routes to #create' do
-      expect(post: '/reservations').to route_to('reservations#create')
+      expect(post: '/reservations').to route_to('reservations#create', format: :json)
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/reservations/1').to route_to('reservations#update', id: '1')
+      expect(put: '/reservations/1').to route_to('reservations#update', id: '1', format: :json)
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/reservations/1').to route_to('reservations#update', id: '1')
+      expect(patch: '/reservations/1').to route_to('reservations#update', id: '1', format: :json)
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/reservations/1').to route_to('reservations#destroy', id: '1')
+      expect(delete: '/reservations/1').to route_to('reservations#destroy', id: '1', format: :json)
     end
   end
 end
--- spec/routing/videogames_routing_spec.rb
+++ spec/routing/videogames_routing_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 RSpec.describe VideogamesController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/videogames').to route_to('videogames#index')
+      expect(get: '/videogames').to route_to('videogames#index', format: :json)
     end
 
     it 'routes to #show' do
-      expect(get: '/videogames/1').to route_to('videogames#show', id: '1')
+      expect(get: '/videogames/1').to route_to('videogames#show', id: '1', format: :json)
     end
 
     it 'routes to #create' do
-      expect(post: '/videogames').to route_to('videogames#create')
+      expect(post: '/videogames').to route_to('videogames#create', format: :json)
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/videogames/1').to route_to('videogames#update', id: '1')
+      expect(put: '/videogames/1').to route_to('videogames#update', id: '1', format: :json)
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/videogames/1').to route_to('videogames#update', id: '1')
+      expect(patch: '/videogames/1').to route_to('videogames#update', id: '1', format: :json)
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/videogames/1').to route_to('videogames#destroy', id: '1')
+      expect(delete: '/videogames/1').to route_to('videogames#destroy', id: '1', format: :json)
     end
   end
 end
